785be9d47c2ac44a5e9ec77aac96233c8dc19cf0
amd1.7 <amedbakayoko75@gmail.com>
2026-04-14 21:49:54 +0200

    (ADD): -Création serveur GRPC coté golang 'Serveur Principal' création serveur GRPC pour micro-service coté

---
+++ SERVEUR/SERVEUR-PRINCIPAL/client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"Server/p"
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	succ, err := grpc.NewClient(":8081", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Erreur de la connexion au serveur: %v\n", err)
+	}
+
+	c := p.NewTrafiqServiceClient(succ)
+
+	requete := p.InputUser{
+		Ville: "Paris",
+		Bus:   "1",
+		Arret: "Châteleet",
+	}
+
+	reponse, err := c.GetInput(context.Background(), &requete)
+	if err != nil {
+		log.Fatalf("Erreur de transmissions de la DATA: %v\n", err)
+	}
+	log.Printf("TempsInitial: %v| TempsReel: %v", reponse.TempsInitial, reponse.TempsReel)
+
+}
